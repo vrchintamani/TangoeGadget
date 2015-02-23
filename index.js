727c1aaa043eb0b4aea74edf9b761922a00cb750
Sudheep Divakar <sudheepdivakar@gmail.com>
2015-02-23 12:07:11 +0530

    version 1.0

--- index.js
+++ index.js
@@ -1,74 +1,109 @@
-﻿$(function(){
-	$('.tabs .tab-links a').click(function(e)  {
-		var currentAttrValue = $(this).attr('data-tab');
+﻿function LoadContent(){
+
+	$.support.cors = true;
+	// var net = new ActiveXObject ( "WScript.NetWork" );
+	// var loggedUserName = net.UserName
+	// $('#userName').html(net.UserName);
+
+	$.ajax({
+        type: "get",
+		async: false,
+		cache: false,
+        url: 'http://localhost/GadgetAPI/api/Values', //'http://gprasad16430:9999/TangoeTrac/ServletVersion?userName='+loggedUserName,
+		success: function (data) {           	
+			
+			$('#MainContainer').html(data);
+			
+			firstTabData();			
+					
+        },
+		error:function(xhr, status, error){			
+			$('#MainContainer').html('Unable to connect to the server');
+		}
+    });	
+	
+	$("#ticketsTab").click(function(e){	
+		firstTabData();	
+	})
+	
+	function firstTabData()
+	{
+		$.support.cors = true;
+		
 		$.ajax({
-			type: "GET",
-			async: false,
-			url: 'http://10.80.50.234/Gadget/api/' + currentAttrValue,
-			success: function (data) {
-				$('#Tabs').html(data);
-				
-			}
-		});
-		$(this).parent('li').addClass('active').siblings().removeClass('active'); 
-		e.preventDefault();		
-	});	
-	$('#tab1').click();
-});
-
-
-
-// $(function(){
-	// $('.tabs .tab-links a').click(function(e)  {
-		// alert('called')
-        // var currentAttrValue = $(this).attr('href');
-		// $.ajax({
-			// type: "GET",
-			// async: false,
-			// url: 'http://10.80.50.234/Gadget/api/' + currentAttrValue,
-			// success: function (data) {
-				// $('#Tabs').html(data);
-			// }
-		// });			
-		// $(this).parent('li').addClass('active').siblings().removeClass('active'); 
-		// e.preventDefault();	
-	// });		
-// })
-
-
-
-// function vijay(){
-	// debugger;
-	// var txt = "asdasd";
-	// $.ajax({
-        // type: "GET",
-		// async: false,
-        // url: 'http://10.80.50.234/Gadget/api/Gadget/',
-		// success: function (data) {
-            // txt = data;	
-			// // alert(txt);
-			// $('#MainContainer').html(txt);
-			// $('#tab-1').text('Tab 1');
-			// $('#tab-2').text('Tab 2');
-			// $('#tab-3').text('Tab 3');
-			// $('#tab-4').text('Tab 4');	
-			// $('#tab1 p').text('This is tab 1');
-			// $('#tab2 p').text('This is tab 2');
-			// $('#tab3 p').text('This is tab 3');
-			// $('#tab4 p').text('This is tab 4');
-        // }
-    // });	
-	// $('.tabs .tab-links a').click(function(e)  {
-        // var currentAttrValue = $(this).attr('href');
- 
-        // // Show/Hide Tabs
-        // //$('.tabs ' + currentAttrValue).show().siblings().hide();
- 
-        // // Change/remove current tab to active
-        // $(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        // e.preventDefault();
-    // });
+        type: "get",
+		cache : false,
+		async: false,
+        url: 'http://localhost/GadgetAPI/api/Tickets',//'http://gprasad16430:9999/TangoeTrac/ServletVersion?userName='+loggedUserName,
+		success: function (data) {
+          		
+			$('#ticketsTabContent').html(data);
+			
+        },
+		error:function(xhr, status, error){			
+			$('#ticketsTabContent').html("Unable to connect the server");
+		}
+		
+	})
+		
+	}
+	
+	$("#projectsTab").click(function(e){
+	secondTabData();
+	})
+	
+	function secondTabData()
+	{		
+		$.support.cors = true;
+	
+		$.ajax({
+        type: "get",
+		async: false,
+		cache:false,
+        url: 'http://localhost/GadgetAPI/api/Projects',//'http://gprasad16430:9999/TangoeTrac/ServletVersion?userName=admin',
+		success: function (data) {
+          		
+			$('#projectsTabContent').html(data);
+			
+        },
+		error:function(xhr, status, error){			
+			$('#projectsTabContent').html("Unable to connect to the server");
+		}
+		
+	})
+	}
+	
+	$("#backlogTab").click(function(e){
+		thirdTabData();
+	})
+	
+	function thirdTabData()
+	{
+		$.support.cors = true;
+		$.ajax({
+        type: "get",
+		async: false,
+		cache:false,
+        url: 'http://localhost/GadgetAPI/api/Backlog',//'http://gprasad16430:9999/TangoeTrac/ServletVersion?userName=release.prep',
+		success: function (data) {
+            		
+			$('#backlogTabContent').html(data);
+			
+        },
+		error:function(xhr, status, error){			
+			$('#backlogTabContent').html("Unable to connect to the server");
+		}
+		})
+	}
+	
+	setInterval(function() {
+      firstTabData();
+	  secondTabData();
+	  thirdTabData();
+	  
+}, 10000);
+}
 	
 	
-// }
+      
+	
